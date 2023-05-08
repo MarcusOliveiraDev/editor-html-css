e63d2eabdbcab0dc81422a6e004f306571094a82
Marcus Oliveira Dev <devmarcusoliveira@gmail.com>
2023-05-08 10:39:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+ace.require("ace/ext/language_tools");
+const editorhtml = ace.edit("text-var-html");
+editorhtml.setTheme("ace/theme/textmate");
+editorhtml.session.setMode("ace/mode/html");
+
+const editorcss = ace.edit("text-var-css");
+editorcss.setTheme("ace/theme/textmate");
+editorcss.session.setMode("ace/mode/css");
+
+const editorjs = ace.edit("text-var-js");
+editorjs.setTheme("ace/theme/textmate");
+editorjs.session.setMode("ace/mode/javascript");
+
+// enable autocompletion and snippets
+editorhtml.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+    wrap: true
+});
+editorcss.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+    wrap: true
+});
+editorjs.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+    wrap: true
+});
+
 //variaveis que captura o que foi escrito
 let vartexthtml = editorhtml.getValue();
 let vartextcss = editorcss.getValue();
+let vartextjs = editorjs.getValue();
 //variaveis para mostrar o código
 let mostrartext = document.getElementById("text-preview");
 
@@ -8,11 +42,13 @@ let mostrartext = document.getElementById("text-preview");
 function rodar(){
     vartexthtml = editorhtml.getValue();
     vartextcss = editorcss.getValue();
+    vartextjs = editorjs.getValue();
 
-    mostrartext.srcdoc = vartexthtml + '<style>'+vartextcss+'<style>';
+    mostrartext.srcdoc = vartexthtml + '<style>'+vartextcss+'</style>' + '<script>'+vartextjs+'</script>';
 
     vartexthtml = document.getElementById("text-var-html");
     vartextcss = document.getElementById("text-var-css");
+    vartextjs = document.getElementById("text-var-js");
 }
 
 //função baixar o código
